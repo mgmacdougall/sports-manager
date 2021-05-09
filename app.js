640ccd2b685afe04e5a4df6362d04e41254a3d81
Mike MacDougall <MacD>
2021-05-09 08:25:47 -0400

    Add of basic 'views' folder, main page and views directory'

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 /* eslint-disable linebreak-style */
 const express = require('express');
+const path = require('path')
 
 const app = express();
-app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 4040;
 
 app.get('/', (req, res) => {
-  res.send("index.html");
+  res.render('home', {pageName:"Home Page"})
 });
 
 app.listen(PORT, () => console.log(`App started on port ${PORT}`));
--- public/js/coaches.js
+++ public/js/coaches.js
@@ -1,4 +1,56 @@
+const news = document.getElementById("news");
+const playerElemeent = document.getElementById("player")
 
+const newsData = [
+      {
+        "id": 1,
+        "title":"test",
+        "content": "There was a foo"
+      },
+      
+      {
+        "id": 2,
+        "title":"test1",
+        "content": "There was a foo"
+      },
+]
+
+
+const players = [
+  {
+    "number": 1,
+    "fName": "Mike",
+    "lName": "M",
+    "Position": "Right FB"
+  },
+  {
+    "number": 2,
+    "fName": "Mike",
+    "lName": "M",
+    "Position": "Left FB"
+  },
+  {
+    "number": 3,
+    "fName": "Mike",
+    "lName": "M",
+    "Position": "Goalie"
+  },
+]
+
+const updateNews = (cb) =>{
+  for(let item of newsData){
+     news.insertAdjacentHTML('beforeend',`<div><span class="news-data">${item.title}</span></div>`)
+  }
+  cb()
+}
+
+
+const loadPlayers = () =>{
+  for(let player of players){
+    playerElemeent.insertAdjacentHTML('beforeend',`<div><span class="player-inf">${player.fName}</span></div>`)
+  }
+
+}
 // Call this to load the current contents for each of the cards
-const doSomething = () => console.log('hi from coaches')
-window.onload = doSomething
+// const doSomething = () => console.log('hi from coaches')
+window.onload = updateNews(loadPlayers);
