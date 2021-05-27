cb28db14ffa6b6099cc05b521220d45296b5cadd
Mike MacDougall <MacD>
2021-05-26 20:27:09 -0400

    Add of Admin news page.  Add of bootstrap to project. Remove of index.html. Add of jquery

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const path = require('path');
 
-
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -16,7 +18,7 @@ const PORT = process.env.PORT || 4040;
 const links=[
 	{
 		linkName: "Home",
-		linkURL: "home.html"
+		linkURL: "/"
 	},
 	{
 		linkName: "Coaches",
@@ -26,22 +28,41 @@ const links=[
 		linkName: "Login",
 		linkURL: "login.html"
 	},
-		{
-			linkName: "Teams",
-			linkURL: "teams.html"
-		}
+	{
+		linkName: "Teams",
+		linkURL: "teams.html"
+	},
+	{
+		linkName: "Club Admin",
+		linkURL: "/clubAdmin"
+	}
 ]
 
 app.get('/', (req, res) => {
 	const news = [
-		{ title: 'Club news - Red Team', desc: "Welcome to today's news.  Team Red takes lead" },
-		{ title: 'Boys 15-16 Rep Team', desc: 'Rep team off to Windsor for weekend.' },
-		{ title: 'Girls 15-16 Rep Team', desc: 'Big win on the weekend against Sarnia.  Takes 2nd place in the Tourney.' },
-		{ title: 'Club news - Green Team', desc: 'Game vs. Blue team - canceled.' },
+		{ title: 'Club news - Red Team', tagLine:"Red Moves Up",  desc: "Welcome to today's news.  Team Red takes lead" },
+		{ title: 'Boys 15-16 Rep Team', tagLine:"Boys rep team travelling", desc: 'Rep team off to Windsor for weekend.' },
+		{ title: 'Girls 15-16 Rep Team', tagLine: "Girls rep team on the road", desc: 'Big win on the weekend against Sarnia.  Takes 2nd place in the Tourney.' },
+		{ title: 'Club news - Green Team', tagLine: "Green vs. Blue cancellation: ", desc: 'Green vs. Blue team - canceled.' },
 	];
 
 	res.render('home', { pageName: 'Home', news: news, links: links });
 });
+
+app.get('/teams', (req,res)=>{
+	const teams =[{
+		name: "Red",
+		coach: "Tim Burton",
+		homePage: "red"
+	},
+	{
+		name:"Blue",
+		coach: "Jon Bill",
+		homePage: "blue"
+	}]
+	res.render("teams", {pageName: "Club Teams", teams})
+
+})
 
 app.get('/team/:name', (req, res) => {
 	const { name } = req.params;
@@ -71,6 +92,17 @@ app.get('/team/:name', (req, res) => {
 
 	res.render('team', { pageName: name, info: team.news,  links: links });
 });
+
+
+app.get('/clubAdmin',(req,res)=>{
+	res.render('admin', { pageName: "Club Administration",links: links });
+})
+
+app.post('/clubAdmin', (req,res)=>{
+
+	console.log(req.body)
+	res.send('done')
+})
 
 app.listen(PORT, () => console.log(`App started on port ${PORT}`));
 
